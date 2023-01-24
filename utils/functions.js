f92ce1961a7d153228b39520381f9cab2f8e63e6
iTzArshia <iTz.Arshia80@Gmail.com>
2023-01-25 01:08:45 +0330

    Update functions.js
    Add new function

--- utils/functions.js
+++ utils/functions.js
@@ -1,5 +1,9 @@
 module.exports = {
 
+	numberWithCommas: function (number) { // 1000 to 1,000
+		return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	},
+    
 	queueStatus: function (queue) {// Queue status template
 		return `**Volume:** \`${queue.volume}%\` | **Filters:** \`${queue.filters.names.join(', ') || 'OFF'}\` | **Loop:** \`${queue.repeatMode ? queue.repeatMode === 2 ? 'All Queue' : 'This Song' : 'OFF'}\` | **Autoplay:** \`${queue.autoplay ? 'ON' : 'OFF'}\``;
 	},
