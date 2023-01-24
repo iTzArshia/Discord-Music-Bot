8b99ad24a5a8c622e1a4e4da106e61b28a59cd24
iTzArshia <iTz.Arshia80@Gmail.com>
2023-01-24 22:45:11 +0330

    Create stop.js

---
+++ commands/messages/stop.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  name: "Stop",
+  description: "Stops the queue",
+  memberVoice: true,
+  botVoice: true,
+  sameVoice: true,
+  queueNeeded: true,
+
+  async execute(client, message, args, cmd, memberVC, botVC, queue) {
+
+    await queue.stop()
+
+    const stopEmbed = new Discord.EmbedBuilder()
+      .setColor(config.mainColor)
+      .setDescription('Stopped playing.');
+
+    return message.reply({ embeds: [stopEmbed] });
+
+  },
+
+};
