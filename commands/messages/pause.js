a5255b156ee31b41c1003867affc15838e989d22
iTzArshia <iTz.Arshia80@Gmail.com>
2023-01-24 23:19:44 +0330

    Create Pause/Resume Commands

---
+++ commands/messages/pause.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  name: "Pause",
+  aliases: ["hold"],
+  description: "Pauses the current song",
+  memberVoice: true,
+  botVoice: true,
+  sameVoice: true,
+  queueNeeded: true,
+
+  async execute(client, message, args, cmd, memberVC, botVC, queue) {
+
+    if (queue.paused) {
+
+      const pauseEmbed = new Discord.EmbedBuilder()
+        .setColor(config.mainColor)
+        .setDescription("Queue is already paused.");
+
+      return message.reply({ embeds: [pauseEmbed] });
+
+    };
+
+    await queue.pause()
+
+    const pauseEmbed = new Discord.EmbedBuilder()
+      .setColor(config.mainColor)
+      .setDescription("Paused the song for you.");
+
+    return message.reply({ embeds: [pauseEmbed] });
+
+  },
+
+};
---
+++ commands/messages/resume.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  name: "Resume",
+  aliases: ["unpause"],
+  description: "Resumes the current song",
+  memberVoice: true,
+  botVoice: true,
+  sameVoice: true,
+  queueNeeded: true,
+
+  async execute(client, message, args, cmd, memberVC, botVC, queue) {
+
+    if (!queue.paused) {
+
+      const pauseEmbed = new Discord.EmbedBuilder()
+        .setColor(config.mainColor)
+        .setDescription("Queue isn't paused.");
+
+      return message.reply({ embeds: [pauseEmbed] });
+
+    };
+
+    await queue.resume()
+
+    const pauseEmbed = new Discord.EmbedBuilder()
+      .setColor(config.mainColor)
+      .setDescription("Resumed the song for you.");
+
+    return message.reply({ embeds: [pauseEmbed] });
+
+  },
+
+};
