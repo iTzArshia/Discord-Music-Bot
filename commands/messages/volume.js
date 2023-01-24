d3e94c73ea21d791124d81969f7b872905fe98f0
iTzArshia <iTz.Arshia80@Gmail.com>
2023-01-24 21:42:56 +0330

    Create volume.js

---
+++ commands/messages/volume.js
@@ -0,0 +1,78 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  name: "volume",
+  aliases: ['v', 'set', 'set-volume'],
+  description: "Sets the player volume",
+
+  async execute(client, message, args, cmd) {
+
+    const memberVoiceChannel = message.member.voice.channel;
+    if (!memberVoiceChannel) {
+
+      const inVoiceEmbed = new Discord.EmbedBuilder()
+        .setColor(config.errorColor)
+        .setDescription('You are not connected to any Voice Channel.');
+
+      return message.reply({ embeds: [inVoiceEmbed] });
+
+    };
+
+    const botVoiceChannel = message.guild.members.me.voice.channel;
+    if (!botVoiceChannel) {
+
+      const inVoiceEmbed = new Discord.EmbedBuilder()
+        .setColor(config.errorColor)
+        .setDescription('I\'m not connected to any Voice Chnanel.');
+
+      return message.reply({ embeds: [inVoiceEmbed] });
+
+    };
+
+    if (memberVoiceChannel.id !== botVoiceChannel.id) {
+
+      const inVoiceEmbed = new Discord.EmbedBuilder()
+        .setColor(config.errorColor)
+        .setDescription('You are not connected to my Voice Channel.');
+
+      return message.reply({ embeds: [inVoiceEmbed] });
+
+    };
+
+    const queue = client.distube.getQueue(message.guild)
+    if (!queue) {
+
+      const noQueueEmbed = new Discord.EmbedBuilder()
+        .setColor(config.errorColor)
+        .setDescription('I\'m not playing anything right now.');
+
+      return message.reply({ embeds: [noQueueEmbed] });
+
+    };
+
+    const volume = parseInt(args[0]);
+    if (isNaN(volume)) {
+
+      const notValidNumberEmbed = new Discord.EmbedBuilder()
+        .setColor(config.errorColor)
+        .setDescription('Please enter a valid number.');
+
+      return message.reply({ embeds: [notValidNumberEmbed] });
+
+    };
+
+    await queue.setVolume(volume);
+
+    const status = queue => `**Volume:** \`${queue.volume}%\` | **Filters:** \`${queue.filters.names.join(', ') || 'OFF'}\` | **Loop:** \`${queue.repeatMode ? queue.repeatMode === 2 ? 'All Queue' : 'This Song' : 'OFF'}\` | **Autoplay:** \`${queue.autoplay ? 'ON' : 'OFF'}\``;
+
+    const volumeEmbed = new Discord.EmbedBuilder()
+      .setColor(config.mainColor)
+      .setTitle('Volume Changed')
+      .setDescription(`Volume set to \`${volume}\`\n\n${status(queue)}`)
+
+    return message.reply({ embeds: [volumeEmbed] });
+
+  },
+
+};
