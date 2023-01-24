87d9465a8804d5325b221730518f4d6a72cb235e
iTzArshia <iTz.Arshia80@Gmail.com>
2023-01-25 01:09:03 +0330

    Update nowplaying.js
    Add song information

--- commands/messages/nowplaying.js
+++ commands/messages/nowplaying.js
@@ -15,7 +15,25 @@ module.exports = {
 
     const nowEmbed = new Discord.EmbedBuilder()
       .setColor(config.mainColor)
-      .setDescription(`**Now playing:** ${queue.songs[0].name} (${queue.songs[0].formattedDuration})\n\n${func.queueStatus(queue)}`)
+      .setDescription(`**Now playing:** [${queue.songs[0].name} (${queue.songs[0].formattedDuration})](${queue.songs[0].url})\n\n${func.queueStatus(queue)}`)
+      .addFields(
+        {
+          name: '👀 Views:',
+          value: `${func.numberWithCommas(queue.songs[0].views)}`,
+          inline: true
+        },
+        {
+          name: '👍🏻 Likes:',
+          value: `${func.numberWithCommas(queue.songs[0].likes)}`,
+          inline: true
+        },
+        {
+          name: '👎🏻 DisLikes:',
+          value: `${func.numberWithCommas(queue.songs[0].dislikes)}`,
+          inline: true
+        },
+      )
+      .setThumbnail(queue.songs[0]?.thumbnail)
       .setFooter({
         text: `Requested by ${queue.songs[0].user.tag}`,
         iconURL: queue.songs[0].user.displayAvatarURL({ size: 1024 })
