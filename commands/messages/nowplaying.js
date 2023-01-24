86ee23a8658644ee81bbe20d35e932aff3abea7e
iTzArshia <iTz.Arshia80@Gmail.com>
2023-01-24 21:43:02 +0330

    Update nowplaying.js

--- commands/messages/nowplaying.js
+++ commands/messages/nowplaying.js
@@ -51,10 +51,12 @@ module.exports = {
 
     };
 
+    const status = queue => `**Volume:** \`${queue.volume}%\` | **Filters:** \`${queue.filters.names.join(', ') || 'OFF'}\` | **Loop:** \`${queue.repeatMode ? queue.repeatMode === 2 ? 'All Queue' : 'This Song' : 'OFF'}\` | **Autoplay:** \`${queue.autoplay ? 'ON' : 'OFF'}\``;
+
     const nowEmbed = new Discord.EmbedBuilder()
       .setColor(config.mainColor)
       .setTitle('Now Playing')
-      .setDescription(`**I'm playing:** ${queue.songs[0].name} (${queue.songs[0].formattedDuration})`)
+      .setDescription(`**I'm playing:** ${queue.songs[0].name} (${queue.songs[0].formattedDuration})\n\n${status(queue)}`)
       .setFooter({
         text: `Requested by ${queue.songs[0].user.tag}`,
         iconURL: queue.songs[0].user.displayAvatarURL({ size: 1024 })
