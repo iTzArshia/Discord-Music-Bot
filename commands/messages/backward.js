fd20f192e607031b843612e8396c44041fc3c759
iTzArshia <iTz.Arshia80@Gmail.com>
2023-01-25 13:46:58 +0330

    Rename Rewind to Backward

---
+++ commands/messages/backward.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  name: "backward",
+  aliases: ["bw", "rewind"],
+  description: "-",
+  memberVoice: true,
+  botVoice: true,
+  sameVoice: true,
+  queueNeeded: true,
+
+  async execute(client, message, args, cmd, memberVC, botVC, queue) {
+   
+    const time = Number(args[0]);
+
+    if (!args[0] || isNaN(time)) {
+
+      const noArgsEmbed = new Discord.EmbedBuilder()
+        .setColor(config.errorColor)
+        .setDescription('Please provide time (in seconds) to go backward!\n**Example:** `10` for 10 seconds backward.');
+
+      return await message.reply({ embeds: [noArgsEmbed] });
+
+    };
+
+    try {
+
+      await queue.seek(queue.currentTime - time);
+
+      const seekEmbed = new Discord.EmbedBuilder()
+        .setColor(config.mainColor)
+        .setDescription(`Backwarded the song for ${time} seconds.`);
+
+      return await message.reply({ embeds: [seekEmbed] });
+
+    } catch (error) {
+
+      const errorEmbed = new Discord.EmbedBuilder()
+        .setColor(config.errorColor)
+        .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message);
+
+      return await message.reply({ embeds: [errorEmbed] });
+
+    };
+
+  },
+
+};
