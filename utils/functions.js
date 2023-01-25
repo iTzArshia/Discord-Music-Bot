82a121ea5496a1b6d57039e0f4fdab90b90c79da
iTzArshia <iTz.Arshia80@Gmail.com>
2023-01-25 10:44:25 +0330

    Update functions.js
    Create new function

--- utils/functions.js
+++ utils/functions.js
@@ -1,5 +1,13 @@
 module.exports = {
 
+	suffix: function (number) { // 1 => 1st, 2 => 2nd, 3 => 3rd, 4 => 4th...
+		let j = number % 10, k = number % 100;
+		if (j === 1 && k !== 11) return number + "st";
+		if (j === 2 && k !== 12) return number + "nd";
+		if (j === 3 && k !== 13) return number + "rd";
+		return number + "th";
+	},
+
 	numberWithCommas: function (number) { // 1000 to 1,000
 		return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	},
