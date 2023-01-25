85cae3d04409c57d41d74bf72fea7f0dbee383ee
iTzArshia <iTz.Arshia80@Gmail.com>
2023-01-25 23:15:13 +0330

    Update help.js
    Fix Footer

--- commands/messages/help.js
+++ commands/messages/help.js
@@ -19,7 +19,7 @@ module.exports = {
         iconURL: client.user.displayAvatarURL({ size: 1024 })
       })
       .setDescription(client.MessageCommands.map(c => `> \`${config.Prefix}${c.name}\` \`(${c.aliases?.map(a => `${config.Prefix}${a}`)?.join(' / ') || 'No Aliases'})\`\n> *${c.description}*`).join('\n\n'))
-      .setFooter({ name: 'Developed by iTz Arshia#7650 https://github.com/iTzArshia/iTz-DJ'})
+      .setFooter({ text: 'Developed by iTz Arshia#7650 https://github.com/iTzArshia/iTz-DJ' });
 
     return await message.reply({ embeds: [helpEmbed] });
 
