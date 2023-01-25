1f761d1d10849592470352d34879669f5ee5765c
iTzArshia <iTz.Arshia80@Gmail.com>
2023-01-25 10:44:09 +0330

    Create ping.js

---
+++ commands/messages/ping.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+    name: "Ping",
+    aliases: ["latency"],
+    description: "Shows the bot\'s latency.",
+    memberVoice: false,
+    botVoice: false,
+    sameVoice: false,
+    queueNeeded: false,
+
+    async execute(client, message, args, cmd, memberVC, botVC, queue) {
+
+        const embed = new Discord.EmbedBuilder()
+            .setColor(config.mainColor)
+            .setAuthor({
+                name: `Pong!`,
+                iconURL: client.user.displayAvatarURL({ size: 1024 })
+            })
+            .addFields(
+                {
+                    name: `📡 Ping:`,
+                    value: `${client.ws.ping}ms`,
+                    inline: true
+                },
+                {
+                    name: `💾 Memory:`,
+                    value: `${func.numberWithCommas(Math.round((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)))}MB`,
+                    inline: true
+                },
+                {
+                    name: `⏳ Uptime:`,
+                    value: `<t:${Math.trunc(client.readyTimestamp / 1000)}:D> | <t:${Math.trunc(client.readyTimestamp / 1000)}:R>`,
+                    inline: false
+                },
+            );
+
+        return await message.reply({ embeds: [embed] });
+
+    },
+
+};
