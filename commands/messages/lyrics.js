e2a68e695a527fff02c54af1e5c5cc8fef6af6ed
H00z3xD3v <h00z3xx@gmail.com>
2023-01-26 22:29:02 +0330

    ye dasti tavasote itzarshia dade shod

--- commands/messages/lyrics.js
+++ commands/messages/lyrics.js
@@ -1,58 +1,51 @@
 const axios = require('axios');
 const Discord = require('discord.js');
 const config = require('../../config.json');
-const { json } = require('@distube/yt-dlp');
 
 module.exports = {
     name: "lyrics",
-    aliases: ["lyric", "lyr"],
-    description: "Search you the lyrics!",
-    memberVoice: false,
-    botVoice: false,
-    sameVoice: false,
-    queueNeeded: false,
+    aliases: ["ly", "lyr", "lyric", "text"],
+    description: "Shows the current playing song's lyrics",
+    memberVoice: true,
+    botVoice: true,
+    sameVoice: true,
+    queueNeeded: true,
 
-    async execute(client, message, args, cmd) {
-        const string = args.join(' ');
+    async execute(client, message, args, cmd, memberVC, botVC, queue) {
 
-        if (!string) {
+        const response = await axios.get(`https://some-random-api.ml/lyrics?title="${queue.songs[0].name}"`);
+        const data = response?.data;
+        
+        if (data) {
 
-            const stringEmbed = new Discord.EmbedBuilder()
-                .setColor(config.ErrorColor)
-                .setDescription("Please enter a song name to search the lyrics.")
+            const lyricsEmbed = new Discord.EmbedBuilder()
+                .setColor(config.MainColor)
+                .setTitle(`${queue.songs[0].name}'s Lyrics`)
+                .setDescription(data.lyrics)
+                .setThumbnail(queue.songs[0]?.thumbnail)
                 .setFooter({
                     text: `Commanded by ${message.author.tag}`,
                     iconURL: message.author.displayAvatarURL({ size: 1024 })
                 });
 
-            return await message.reply({ embeds: [stringEmbed] });
+            return await message.reply({ embeds: [lyricsEmbed] });
+
+        } else {
+
+            const lyricsEmbed = new Discord.EmbedBuilder()
+                .setColor(config.ErrorColor)
+                .setTitle(`${queue.songs[0].name}'s Lyrics`)
+                .setDescription(`I can't find ${queue.songs[0].name}'s lyrics`)
+                .setThumbnail(queue.songs[0]?.thumbnail)
+                .setFooter({
+                    text: `Commanded by ${message.author.tag}`,
+                    iconURL: message.author.displayAvatarURL({ size: 1024 })
+                });
+
+            return await message.reply({ embeds: [lyricsEmbed] });
 
         };
 
-        axios.get('https://some-random-api.ml/lyrics?title="' + string + '"')
-            .then(json => {
-                try {
-                    const Lyrics = new Discord.EmbedBuilder()
-                        .setColor(config.MainColor)
-                        .setTitle("**" + json.data.author + " - " + json.data.title + "**")
-                        .setDescription(json.data.lyrics)
-                        .setThumbnail(json.data.thumbnail.genius)
-                        .setFooter({
-                            text: `Commanded by ${message.author.tag}`,
-                            iconURL: message.author.displayAvatarURL({ size: 1024 })
-                        });
-                    return message.reply({ embeds: [Lyrics] });
-                } catch (error) {
-                    const stringEmbed = new Discord.EmbedBuilder()
-                        .setColor(config.ErrorColor)
-                        .setDescription(json.data.error)
-                        .setFooter({
-                            text: `Commanded by ${message.author.tag}`,
-                            iconURL: message.author.displayAvatarURL({ size: 1024 })
-                        })
-                    return message.reply({ embeds: [stringEmbed] });
-                }
-
-            });
     }
-}
+
+};
