089351de4dac317602c460c32f13562ce8b61798
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-10 12:33:55 +0330

    Create forward.js

---
+++ commands/interactions/forward.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  data: new Discord.SlashCommandBuilder()
+    .setName("forward").setDescription("forwards the playing song")
+    .addNumberOption(option => option
+      .setName('time')
+      .setDescription('Time to go forward in seconds')
+      .setRequired(true)
+    ),
+  memberVoice: true,
+  botVoice: true,
+  sameVoice: true,
+  queueNeeded: true,
+
+  async execute(client, interaction, memberVC, botVC, queue) {
+
+    await interaction.deferReply();
+
+    const time = Number(interaction.options.getNumber('time'));
+
+    try {
+
+      await queue.seek(queue.currentTime + time);
+
+      const seekEmbed = new Discord.EmbedBuilder()
+        .setColor(config.MainColor)
+        .setDescription(`forwarded the song for ${time} seconds.`)
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });
+
+      return await interaction.reply({ embeds: [seekEmbed] });
+
+    } catch (error) {
+
+      const errorEmbed = new Discord.EmbedBuilder()
+        .setColor(config.ErrorColor)
+        .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message)
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });
+
+      return await interaction.reply({ embeds: [errorEmbed] });
+
+    };
+
+  },
+
+};
