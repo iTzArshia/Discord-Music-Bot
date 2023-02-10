cf90e90c072b65a46ea2cf0fe83780421b955f76
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-10 13:40:32 +0330

    Create skip.js

---
+++ commands/interactions/skip.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  data: new Discord.SlashCommandBuilder()
+    .setName("skip")
+    .setDescription("Skips the current song"),
+  memberVoice: true,
+  botVoice: true,
+  sameVoice: true,
+  queueNeeded: true,
+
+  async execute(client, interaction, memberVC, botVC, queue) {
+
+    await interaction.deferReply();
+
+    try {
+
+      await queue.skip();
+
+      const skippedEmbed = new Discord.EmbedBuilder()
+        .setColor(config.MainColor)
+        .setDescription("Skipping to the next song.")
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });;
+
+      return await interaction.reply({ embeds: [skippedEmbed] });
+
+    } catch (error) {
+
+      const errorEmbed = new Discord.EmbedBuilder()
+        .setColor(config.ErrorColor)
+        .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message)
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });
+
+      return await interaction.reply({ embeds: [errorEmbed] });
+
+    };
+
+  },
+
+};
