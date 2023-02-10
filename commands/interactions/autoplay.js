6f91985b05257ade330c525198978ba8f36b8bce
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-10 12:12:27 +0330

    Create autoplay.js

---
+++ commands/interactions/autoplay.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+const func = require('../../utils/functions');
+const config = require('../../config.json');
+
+module.exports = {
+    data: new Discord.SlashCommandBuilder()
+        .setName("autoplay")
+        .setDescription("Toggles auto play"),
+    memberVoice: true,
+    botVoice: true,
+    sameVoice: true,
+    queueNeeded: true,
+
+    async execute(client, interaction, memberVC, botVC, queue) {
+    
+        await interaction.deferReply();
+
+        try {
+
+            const autoPlayState = await queue.toggleAutoplay();
+
+            const autoplayEmbed = new Discord.EmbedBuilder()
+                .setColor(config.MainColor)
+                .setDescription(`Auto Play mode changed to \`${autoPlayState ? "ON" : "OFF"}\`\n\n${func.queueStatus(queue)}`)
+                .setFooter({
+                    text: `Commanded by ${interaction.user.tag}`,
+                    iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+                });
+
+            return await message.editReply({ embeds: [autoplayEmbed] });
+
+        } catch (error) {
+
+            const errorEmbed = new Discord.EmbedBuilder()
+                .setColor(config.ErrorColor)
+                .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message)
+                .setFooter({
+                    text: `Commanded by ${interaction.user.tag}`,
+                    iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+                });
+
+            return await interaction.editReply({ embeds: [errorEmbed] });
+
+        };
+
+    },
+
+};
