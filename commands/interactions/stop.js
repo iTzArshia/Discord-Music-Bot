13c6c4091d8b7da8a5cf375339d0ce688e5251c2
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-10 13:41:31 +0330

    Create stop.js

---
+++ commands/interactions/stop.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  data: new Discord.SlashCommandBuilder()
+    .setName("stop")
+    .setDescription("Stops the queue"),
+  memberVoice: true,
+  botVoice: true,
+  sameVoice: true,
+  queueNeeded: true,
+
+  async execute(client, interaction, memberVC, botVC, queue) {
+  
+    await interaction.deferReply();
+
+    try {
+
+      await queue.stop();
+
+      const stopEmbed = new Discord.EmbedBuilder()
+        .setColor(config.MainColor)
+        .setDescription("Stopped playing.")
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });
+
+      return await interaction.editReply({ embeds: [stopEmbed] });
+
+    } catch (error) {
+
+      const errorEmbed = new Discord.EmbedBuilder()
+        .setColor(config.ErrorColor)
+        .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message)
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });
+
+      return await interaction.editReply({ embeds: [errorEmbed] });
+
+    };
+
+  },
+
+};
