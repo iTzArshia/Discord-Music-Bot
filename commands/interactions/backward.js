dd5903800cd66a8962f80eacd57188f871f80160
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-10 12:12:36 +0330

    Create backward.js

---
+++ commands/interactions/backward.js
@@ -0,0 +1,54 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  data: new Discord.SlashCommandBuilder()
+    .setName("backward")
+    .setDescription("backwards the playing song")
+    .addNumberOption(option => option
+      .setName('time')
+      .setDescription('Provide time in seconds')
+      .setRequired(true)
+    ),
+  memberVoice: true,
+  botVoice: true,
+  sameVoice: true,
+  queueNeeded: true,
+
+  async execute(client, interaction, memberVC, botVC, queue) {
+
+    await interaction.deferReply();
+
+    const time = interaction.options.getNumber('time');
+
+    try {
+
+      await queue.seek(queue.currentTime - time);
+
+      const seekEmbed = new Discord.EmbedBuilder()
+        .setColor(config.MainColor)
+        .setDescription(`Backwarded the song for ${time} seconds.`)
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });
+
+      return await interaction.editReply({ embeds: [seekEmbed] });
+
+    } catch (error) {
+
+      const errorEmbed = new Discord.EmbedBuilder()
+        .setColor(config.ErrorColor)
+        .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message)
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });
+
+      return await interaction.editReply({ embeds: [errorEmbed] });
+
+    };
+
+  },
+
+};
