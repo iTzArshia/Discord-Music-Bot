579189c3436346d4dd056022660dc3ae23a898d8
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-10 13:32:48 +0330

    Create playskip.js

---
+++ commands/interactions/playskip.js
@@ -0,0 +1,58 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  data: new Discord.SlashCommandBuilder()
+    .setName("playskip")
+    .setDescription("Plays the song and skips current song")
+    .addStringOption(option => option
+      .setName('query')
+      .setDescription('Enter song name or playlist list')
+      .setRequired(true)
+    ),
+  memberVoice: true,
+  botVoice: false,
+  sameVoice: true,
+  queueNeeded: false,
+
+  async execute(client, interaction, memberVC, botVC, queue) {
+   
+    await interaction.deferReply({ ephemeral: true });
+
+    const query = interaction.options.getString('query');
+   
+    const searchEmbed = new Discord.EmbedBuilder()
+      .setColor(config.MainColor)
+      .setDescription("Searching...")
+      .setFooter({
+        text: `Commanded by ${interaction.user.tag}`,
+        iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+      });
+
+    await interaction.editReply({ embeds: [searchEmbed] });
+
+    try {
+
+      await client.distube.play(memberVC, query, {
+        member: interaction.member,
+        textChannel: interaction.channel,
+        skip: true
+      });
+
+    } catch (error) {
+
+      const errorEmbed = new Discord.EmbedBuilder()
+        .setColor(config.ErrorColor)
+        .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message)
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });
+
+      return await interaction.editReply({ embeds: [errorEmbed] });
+
+    };
+
+  },
+
+};
