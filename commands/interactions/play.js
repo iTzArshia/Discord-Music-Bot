36273aa106e99e83288d7a1897260b022a0361b4
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-10 13:30:10 +0330

    Create play.js

---
+++ commands/interactions/play.js
@@ -0,0 +1,60 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  data: new Discord.SlashCommandBuilder()
+    .setName("play")
+    .setDescription("Plays song for you")
+    .addStringOption(option => option
+      .setName('query')
+      .setDescription('Enter song name or playlist list')
+      .setRequired(true)
+    ),
+  memberVoice: true,
+  botVoice: false,
+  sameVoice: true,
+  queueNeeded: false,
+
+  async execute(client, interaction, memberVC, botVC, queue) {
+
+    await interaction.deferReply({ ephemeral: true });
+
+    const string = interaction.options.getString('name');
+
+    const searchEmbed = new Discord.EmbedBuilder()
+      .setColor(config.MainColor)
+      .setDescription("Searching...")
+      .setFooter({
+        text: `Commanded by ${interaction.user.tag}`,
+        iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+      });
+
+    await interaction.editReply({ embeds: [searchEmbed] });
+
+    try {
+
+      await client.distube.play(memberVC, string, {
+        member: interaction.member,
+        textChannel: interaction.channel
+      });
+
+      await interaction.deleteReply();
+      
+    } catch (error) {
+
+      const errorEmbed = new Discord.EmbedBuilder()
+        .setColor(config.ErrorColor)
+        .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message)
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });
+
+      return await interaction.editReply({ embeds: [errorEmbed] });
+
+    };
+
+  },
+
+};
