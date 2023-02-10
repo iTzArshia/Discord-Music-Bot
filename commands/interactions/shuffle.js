4f8fe34ec59c01beeb70ea197a56f9f15e7d6347
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-10 13:40:22 +0330

    Create shuffle.js

---
+++ commands/interactions/shuffle.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+const config = require('../../config.json');
+
+module.exports = {
+  data: new Discord.SlashCommandBuilder()
+    .setName("shuffle")
+    .setDescription("Shuffles the queue song"),
+  memberVoice: true,
+  botVoice: true,
+  sameVoice: true,
+  queueNeeded: true,
+
+  async execute(client, interaction, memberVC, botVC, queue) {
+  
+    await interaction.deferReply();
+
+    try {
+
+      await queue.shuffle();
+
+      const shuffleEmbed = new Discord.EmbedBuilder()
+        .setColor(config.MainColor)
+        .setDescription("Shuffled songs in the queue")
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });;
+
+      return await interaction.reply({ embeds: [shuffleEmbed] });
+
+    } catch (error) {
+
+      const errorEmbed = new Discord.EmbedBuilder()
+        .setColor(config.ErrorColor)
+        .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message)
+        .setFooter({
+          text: `Commanded by ${interaction.user.tag}`,
+          iconURL: interaction.user.displayAvatarURL({ size: 1024 })
+        });
+
+      return await interaction.reply({ embeds: [errorEmbed] });
+
+    };
+
+  },
+
+};
