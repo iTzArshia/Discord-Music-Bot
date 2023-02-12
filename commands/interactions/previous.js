0dd428b61bb5ebd74191e1de928a4ab261a21c18
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-12 12:05:22 +0330

    Update previous.js
    Update command description

--- commands/interactions/previous.js
+++ commands/interactions/previous.js
@@ -4,7 +4,7 @@ const config = require('../../config.json');
 module.exports = {
   data: new Discord.SlashCommandBuilder()
     .setName("previous")
-    .setDescription("Plays previous song"),
+    .setDescription("Plays previous song."),
   memberVoice: true,
   botVoice: true,
   sameVoice: true,
