2a7a0c23606df4a3ce6b574a0682307d396a51e3
iTzArshia <iTz.Arshia80@Gmail.com>
2023-02-12 12:07:02 +0330

    Update previous.js
    Update command aliases and description

--- commands/messages/previous.js
+++ commands/messages/previous.js
@@ -3,8 +3,8 @@ const config = require('../../config.json');
 
 module.exports = {
   name: "Previous",
-  aliases: ["b", "back"],
-  description: "Plays previous song",
+  aliases: ["B", "Back"],
+  description: "Plays previous song.",
   memberVoice: true,
   botVoice: true,
   sameVoice: true,
