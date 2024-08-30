e387738e4a6beabd8f02fb56382d16f5c96e04ae
iTz Arshia <89854127+iTzArshia@users.noreply.github.com>
2024-08-30 22:58:28 +0330

    Create voiceStateUpdate.js

---
+++ events/discord/voiceStateUpdate.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const Distube = require("distube");
+const config = require("../../config.json");
+
+module.exports = async (client, oldState, newState) => {
+    if (!oldState?.channel) return;
+    if (client.distubeSettings.leaveOnEmpty) {
+        const connection = client.distube.voices.get(oldState);
+        if (connection && Distube.isVoiceChannelEmpty(oldState)) {
+            const queue = client.distube.queues.get(oldState);
+            const textChannel = queue?.textChannel;
+
+            await connection.leave();
+
+            if (textChannel) {
+                const embed = new Discord.EmbedBuilder()
+                    .setColor(config.ErrorColor)
+                    .setDescription("The voice channel is empty! Leaving the voice channel.");
+
+                await textChannel?.send({ embeds: [embed] });
+            }
+        }
+    }
+};
