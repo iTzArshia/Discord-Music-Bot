c9b1866363fe8dec2a795c370a72cc85266cd68a
iTz Arshia <89854127+iTzArshia@users.noreply.github.com>
2024-08-31 01:01:58 +0330

    Update finishSong.js

--- events/distube/finishSong.js
+++ events/distube/finishSong.js
@@ -2,7 +2,5 @@ const Discord = require("discord.js");
 const config = require("../../config.json");
 
 module.exports = async (client, queue) => {
-    const embed = new Discord.EmbedBuilder().setColor(config.ErrorColor).setDescription("Finish song!");
 
-    await queue.textChannel?.send({ embeds: [embed] });
 };
