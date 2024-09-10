11ea0a5ebc3586e66dff0c91b3c8609dbc7adecd
iTz Arshia <89854127+iTzArshia@users.noreply.github.com>
2024-09-10 10:23:55 +0330

    Update playSong.js
    Remove collector and use interactionCreate event instead
    
    Now because of this buttons wont expire before song end

--- events/distube/playSong.js
+++ events/distube/playSong.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const func = require("../../utils/functions");
-const { row2, row3 } = require("../../utils/components");
 const config = require("../../config.json");
 
 module.exports = async (client, queue, song) => {
@@ -55,229 +54,33 @@ module.exports = async (client, queue, song) => {
     filters.addOptions(options);
     const row1 = new Discord.ActionRowBuilder().addComponents([filters]);
 
-    const reply = await queue.textChannel?.send({
+    const loopSongToggle = new Discord.ButtonBuilder().setCustomId("loop-song").setEmoji("🔂").setStyle(Discord.ButtonStyle.Secondary);
+
+    const previousSong = new Discord.ButtonBuilder().setCustomId("previous").setEmoji("⏮️").setStyle(Discord.ButtonStyle.Secondary);
+
+    const paunseUnpause = new Discord.ButtonBuilder().setCustomId("pauseUnpause").setEmoji("⏯️").setStyle(Discord.ButtonStyle.Secondary);
+
+    const nextSong = new Discord.ButtonBuilder().setCustomId("next").setEmoji("⏭️").setStyle(Discord.ButtonStyle.Secondary);
+
+    const loopQueueToggle = new Discord.ButtonBuilder().setCustomId("loop-queue").setEmoji("🔁").setStyle(Discord.ButtonStyle.Secondary);
+
+    const volumeDown = new Discord.ButtonBuilder().setCustomId("vol-down").setEmoji("🔉").setStyle(Discord.ButtonStyle.Secondary);
+
+    const backward = new Discord.ButtonBuilder().setCustomId("backward").setEmoji("⏪").setStyle(Discord.ButtonStyle.Secondary);
+
+    const stop = new Discord.ButtonBuilder().setCustomId("stop").setEmoji("⏹️").setStyle(Discord.ButtonStyle.Secondary);
+
+    const forward = new Discord.ButtonBuilder().setCustomId("forward").setEmoji("⏩").setStyle(Discord.ButtonStyle.Secondary);
+
+    const volumeUp = new Discord.ButtonBuilder().setCustomId("vol-up").setEmoji("🔊").setStyle(Discord.ButtonStyle.Secondary);
+
+    const row2 = new Discord.ActionRowBuilder().addComponents([loopSongToggle, previousSong, paunseUnpause, nextSong, loopQueueToggle]);
+    const row3 = new Discord.ActionRowBuilder().addComponents([volumeDown, backward, stop, forward, volumeUp]);
+
+    const playingMessage = await queue.textChannel?.send({
         embeds: [embed],
         components: [row1, row2, row3],
     });
 
-    const collector = await reply.createMessageComponentCollector({ time: song.duration * 1000 });
-
-    collector.on("collect", async (int) => {
-        const memberVC = int.member.voice.channel || null;
-        const botVC = int.guild.members.me.voice.channel || null;
-
-        if (memberVC && botVC && memberVC.id !== botVC.id) {
-            const inVoiceEmbed = new Discord.EmbedBuilder().setColor(config.ErrorColor).setDescription("You aren't connected to my Voice Channel.");
-
-            return await int.reply({
-                embeds: [inVoiceEmbed],
-                ephemeral: true,
-            });
-        }
-
-        await int.deferReply();
-
-        try {
-            if (int.customId === "filters") {
-                if (queue.filters.has(int.values[0])) {
-                    await queue.filters.remove(int.values[0]);
-                } else {
-                    await queue.filters.add(int.values[0]);
-                }
-
-                await reply.edit({
-                    components: [row1, row2, row3],
-                });
-
-                const filtersEmbed = new Discord.EmbedBuilder()
-                    .setColor(config.MainColor)
-                    .setDescription(`**Current Queue Filters:** \`${queue.filters.names.join(", ") || "OFF"}\`\n\n${func.queueStatus(queue)}`)
-                    .setFooter({
-                        text: `Commanded by ${int.user.globalName || int.user.username}`,
-                        iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                    });
-
-                return await int.editReply({ embeds: [filtersEmbed] });
-            } else if (int.customId.startsWith("loop")) {
-                const loopState = int.customId.split("-")[1];
-                const currentLoopState = queue.repeatMode;
-                const convertedLoopStates = {
-                    0: "off",
-                    1: "song",
-                    2: "queue",
-                };
-
-                let mode = 0;
-
-                if (convertedLoopStates[currentLoopState] === "off") {
-                    if (loopState === "song") mode = 1;
-                    else if (loopState === "queue") mode = 2;
-                } else {
-                    if (loopState !== convertedLoopStates[currentLoopState]) {
-                        if (loopState === "song") mode = 1;
-                        else if (loopState === "queue") mode = 2;
-                    }
-                }
-
-                mode = await queue.setRepeatMode(mode);
-                mode = mode ? (mode === 2 ? "All Queue" : "This Song") : "OFF";
-
-                const loopEmbed = new Discord.EmbedBuilder()
-                    .setColor(config.MainColor)
-                    .setDescription(`Loop mode changed to \`${mode}\`\n\n${func.queueStatus(queue)}`)
-                    .setFooter({
-                        text: `Commanded by ${int.user.globalName || int.user.username}`,
-                        iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                    });
-
-                return await int.editReply({ embeds: [loopEmbed] });
-            } else if (int.customId === "previous") {
-                await queue.previous();
-
-                const skippedEmbed = new Discord.EmbedBuilder()
-                    .setColor(config.MainColor)
-                    .setDescription("Skipping to the previus song.")
-                    .setFooter({
-                        text: `Commanded by ${int.user.globalName || int.user.username}`,
-                        iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                    });
-
-                await int.editReply({ embeds: [skippedEmbed] });
-
-                return await collector.stop();
-            } else if (int.customId === "pauseUnpause") {
-                if (queue.playing) {
-                    await queue.pause();
-                } else {
-                    await queue.resume();
-                }
-
-                const pauseUnpauseEmbed = new Discord.EmbedBuilder()
-                    .setColor(config.MainColor)
-                    .setDescription(`${queue.playing ? "Resumed" : "Paused"} the song for you.`)
-                    .setFooter({
-                        text: `Commanded by ${int.user.globalName || int.user.username}`,
-                        iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                    });
-
-                return await int.editReply({ embeds: [pauseUnpauseEmbed] });
-            } else if (int.customId === "next") {
-                await queue.skip();
-
-                const skippedEmbed = new Discord.EmbedBuilder()
-                    .setColor(config.MainColor)
-                    .setDescription("Skipping to the next song.")
-                    .setFooter({
-                        text: `Commanded by ${int.user.globalName || int.user.username}`,
-                        iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                    });
-
-                await int.editReply({ embeds: [skippedEmbed] });
-
-                return await collector.stop();
-            } else if (int.customId.startsWith("vol")) {
-                const volumeUpDown = int.customId.split("-")[1];
-
-                if (volumeUpDown === "up") {
-                    
-                    if (queue.volume === 200) {
-
-                        const volumeEmbed = new Discord.EmbedBuilder()
-                    .setColor(config.ErrorColor)
-                    .setDescription("You can't make volume more than `200`")
-                    .setFooter({
-                        text: `Commanded by ${int.user.globalName || int.user.username}`,
-                        iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                    });
-
-                        return await int.editReply({ embeds: [volumeEmbed] });
-                    }
-                    await queue.setVolume(queue.volume + 10);
-
-                }
-
-                else if (volumeUpDown === "down") {
-                    
-                    if (queue.volume === 0) {
-
-                        const volumeEmbed = new Discord.EmbedBuilder()
-                        .setColor(config.ErrorColor)
-                        .setDescription("You can't make volume less than `0`")
-                        .setFooter({
-                            text: `Commanded by ${int.user.globalName || int.user.username}`,
-                            iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                        });
-
-                        return await int.editReply({ embeds: [volumeEmbed] });
-                    }
-                    await queue.setVolume(queue.volume - 10);
-
-                }
-
-                const volumeEmbed = new Discord.EmbedBuilder()
-                    .setColor(config.MainColor)
-                    .setDescription(`Volume changed to \`${queue.volume}\`\n\n${func.queueStatus(queue)}`)
-                    .setFooter({
-                        text: `Commanded by ${int.user.globalName || int.user.username}`,
-                        iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                    });
-
-                return await int.editReply({ embeds: [volumeEmbed] });
-            } else if (int.customId === "backward") {
-                await queue.seek(queue.currentTime - 10);
-
-                const seekEmbed = new Discord.EmbedBuilder()
-                    .setColor(config.MainColor)
-                    .setDescription(`Backwarded the song for 10 seconds.`)
-                    .setFooter({
-                        text: `Commanded by ${int.user.globalName || int.user.username}`,
-                        iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                    });
-
-                return await int.editReply({ embeds: [seekEmbed] });
-            } else if (int.customId === "stop") {
-                await queue.stop();
-                if (client.distubeSettings.leaveOnStop) await queue.voice.leave();
-
-                const stopEmbed = new Discord.EmbedBuilder()
-                    .setColor(config.MainColor)
-                    .setDescription("Stopped playing.")
-                    .setFooter({
-                        text: `Commanded by ${int.user.globalName || int.user.username}`,
-                        iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                    });
-
-                await int.editReply({ embeds: [stopEmbed] });
-
-                return await collector.stop();
-            } else if (int.customId === "forward") {
-                await queue.seek(queue.currentTime + 10);
-
-                const seekEmbed = new Discord.EmbedBuilder()
-                    .setColor(config.MainColor)
-                    .setDescription(`forwarded the song for 10 seconds.`)
-                    .setFooter({
-                        text: `Commanded by ${int.user.globalName || int.user.username}`,
-                        iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                    });
-
-                return await int.editReply({ embeds: [seekEmbed] });
-            }
-        } catch (error) {
-            const errorEmbed = new Discord.EmbedBuilder()
-                .setColor(config.ErrorColor)
-                .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message)
-                .setFooter({
-                    text: `Commanded by ${int.user.globalName || int.user.username}`,
-                    iconURL: int.user.displayAvatarURL({ size: 1024 }),
-                });
-
-            return await int.editReply({ embeds: [errorEmbed] });
-        }
-    });
-
-    collector.on("end", async (collection, reason) => {
-        if (["messageDelete", "messageDeleteBulk"].includes(reason)) return;
-        await reply.edit({ components: [] }).catch(() => null);
-    });
+    client.PlayingMessageID = playingMessage.id;
 };
