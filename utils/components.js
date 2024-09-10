a5c475317d5bbbff554b664ed4d86d3e8bd6a898
iTz Arshia <89854127+iTzArshia@users.noreply.github.com>
2024-09-10 10:22:13 +0330

    Update components.js
    Update Paginations

--- utils/components.js
+++ utils/components.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 
-const paginationStartButton = new Discord.ButtonBuilder().setCustomId("start").setLabel("First").setStyle(Discord.ButtonStyle.Secondary);
+const paginationStartButton = new Discord.ButtonBuilder().setCustomId("page-start").setLabel("First").setStyle(Discord.ButtonStyle.Secondary);
 
-const paginationBackButton = new Discord.ButtonBuilder().setCustomId("back").setLabel("Previous").setStyle(Discord.ButtonStyle.Secondary);
+const paginationBackButton = new Discord.ButtonBuilder().setCustomId("page-back").setLabel("Previous").setStyle(Discord.ButtonStyle.Secondary);
 
-const paginationForwardButton = new Discord.ButtonBuilder().setCustomId("forward").setLabel("Next").setStyle(Discord.ButtonStyle.Secondary);
+const paginationForwardButton = new Discord.ButtonBuilder().setCustomId("page-forward").setLabel("Next").setStyle(Discord.ButtonStyle.Secondary);
 
-const paginationEndButton = new Discord.ButtonBuilder().setCustomId("end").setLabel("Last").setStyle(Discord.ButtonStyle.Secondary);
+const paginationEndButton = new Discord.ButtonBuilder().setCustomId("page-end").setLabel("Last").setStyle(Discord.ButtonStyle.Secondary);
 
 module.exports.paginationStartButton = paginationStartButton;
 module.exports.paginationBackButton = paginationBackButton;
