c58935885f93181b7461c10b9f22b9b8e1ef19a5
iTz Arshia <89854127+iTzArshia@users.noreply.github.com>
2024-09-10 10:35:30 +0330

    Update finishSong.js

--- events/distube/finishSong.js
+++ events/distube/finishSong.js
@@ -2,6 +2,10 @@ module.exports = async (client, queue) => {
     const previousMessage = await queue.textChannel?.messages.fetch(client.PlayingMessageID).catch(() => null);
 
     if (previousMessage) {
-        await previousMessage.edit({ components: [] });
+        if (client.distubeSettings.deleteAfterFinish) {
+            await previousMessage.delete();
+        } else {
+            await previousMessage.edit({ components: [] });
+        }
     }
 };
