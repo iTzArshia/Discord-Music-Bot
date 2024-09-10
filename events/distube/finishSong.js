3fbc70e69aa8a9afbcac735215c1c069b0b299dd
iTz Arshia <89854127+iTzArshia@users.noreply.github.com>
2024-09-10 10:24:30 +0330

    Update finishSong.js
    Remove Message idish after song finished

--- events/distube/finishSong.js
+++ events/distube/finishSong.js
@@ -1,6 +1,7 @@
-const Discord = require("discord.js");
-const config = require("../../config.json");
-
 module.exports = async (client, queue) => {
+    const previousMessage = await queue.textChannel?.messages.fetch(client.PlayingMessageID).catch(() => null);
 
+    if (previousMessage) {
+        await previousMessage.edit({ components: [] });
+    }
 };
