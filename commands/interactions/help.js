11d4386dbec6fb28d98f1f0c1f55ddba3fd8c8c2
iTz Arshia <89854127+iTzArshia@users.noreply.github.com>
2024-09-13 04:21:40 +0330

    Update Help
    Update HELP Command structure
    
    Add Category and usage to Commands data
    Add title to Embeds
    Change footers
    Remove useless returns

--- commands/interactions/help.js
+++ commands/interactions/help.js
@@ -11,22 +11,78 @@ module.exports = {
     async execute(client, interaction, memberVC, botVC, queue) {
         await interaction.deferReply();
 
+        const playCommands = [...client.MessageCommands.values()].filter((command) => command.category === "Play Commands");
+        const songCommands = [...client.MessageCommands.values()].filter((command) => command.category === "Song Commands");
+        const queueCommands = [...client.MessageCommands.values()].filter((command) => command.category === "Queue Commands");
+        const utilitiesCommands = [...client.MessageCommands.values()].filter((command) => command.category === "Utilities Commands");
+
         const helpEmbed = new Discord.EmbedBuilder()
             .setColor(config.MainColor)
-            .setAuthor({
-                name: `${client.user.username} Commands`,
-                iconURL: client.user.displayAvatarURL({ size: 1024 }),
-            })
-            .setDescription(
-                client.MessageCommands.map(
-                    (c) =>
-                        `> \`${config.Prefix}${c.name}\` \`(${c.aliases?.map((a) => `${config.Prefix}${a}`)?.join(" / ") || "No Aliases"})\`\n> *${
-                            c.description
-                        }*`
-                ).join("\n\n")
-            )
-            .setFooter({ text: "Developed by iTz Arshia#7650 https://github.com/iTzArshia/iTz-DJ" });
+            .setTitle("❓ Help")
+            .setFooter({ text: "Developed by iTz Arshia#7650 https://github.com/iTzArshia/Discord-Music-Bot" });
 
-        return await interaction.editReply({ embeds: [helpEmbed] });
+        if (
+            args[0] &&
+            (client.MessageCommands.find((command) => command.name.toLowerCase() === args[0].toLowerCase()) ||
+                client.MessageCommands.find((c) => c.aliases?.map((aliase) => aliase.toLowerCase()).includes(args[0].toLowerCase())))
+        ) {
+            const command =
+                client.MessageCommands.find((command) => command.name.toLowerCase() === args[0].toLowerCase()) ||
+                client.MessageCommands.find((c) => c.aliases?.map((aliase) => aliase.toLowerCase()).includes(args[0].toLowerCase()));
+
+            helpEmbed.setDescription(`Detailed Information about **${command.name}**:`).addFields(
+                {
+                    name: "Command Name:",
+                    value: `\`${config.Prefix}${command.name}\``,
+                    inline: false,
+                },
+                {
+                    name: "Aliases:",
+                    value: command.aliases
+                        ? command.aliases.map((aliase) => `\`${config.Prefix}${aliase}\``).join(" | ")
+                        : "There is no Aliases for this Command",
+                    inline: false,
+                },
+                {
+                    name: "Usage:",
+                    value: config.Prefix + command.usage || "`There is no Usage for this Command`",
+                    inline: false,
+                },
+                {
+                    name: "Description:",
+                    value: command.description || "`There is no Description for this Command`",
+                    inline: false,
+                }
+            );
+        } else {
+            helpEmbed
+                .setDescription(
+                    `You can also use the command \`${config.Prefix}Help CMD\` to access detailed information about available commands and their functionalities.`
+                )
+                .addFields(
+                    {
+                        name: "Play Commands:",
+                        value: playCommands.map((command) => "`" + config.Prefix + command.name + "`").join(", "),
+                        inline: false,
+                    },
+                    {
+                        name: "Song Commands:",
+                        value: songCommands.map((command) => "`" + config.Prefix + command.name + "`").join(", "),
+                        inline: false,
+                    },
+                    {
+                        name: "Queue Commands:",
+                        value: queueCommands.map((command) => "`" + config.Prefix + command.name + "`").join(", "),
+                        inline: false,
+                    },
+                    {
+                        name: "Utilities Commands:",
+                        value: utilitiesCommands.map((command) => "`" + config.Prefix + command.name + "`").join(", "),
+                        inline: false,
+                    }
+                );
+        }
+
+        await interaction.editReply({ embeds: [helpEmbed] });
     },
 };
