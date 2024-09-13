bb75c2477939492e95a7832f80f58ed5cc32a583
iTz Arshia <89854127+iTzArshia@users.noreply.github.com>
2024-09-13 04:23:25 +0330

    Update error.js

--- events/distube/error.js
+++ events/distube/error.js
@@ -1,14 +1,8 @@
 const Discord = require("discord.js");
 const config = require("../../config.json");
 
-module.exports = async (client, textChannel, error) => {
-    console.log(error);
-
-    if (error.message) {
-        const embed = new Discord.EmbedBuilder()
-            .setColor(config.ErrorColor)
-            .setDescription(error.message.length > 4096 ? error.message.slice(0, 4093) + "..." : error.message);
-
-        await textChannel?.send({ embeds: [embed] });
-    }
+module.exports = async (client, error, queue, song) => {
+    const embed = new Discord.EmbedBuilder().setColor(config.ErrorColor).setDescription(`An error encountered: ${error}`);
+    await queue.textChannel?.send({ embeds: [embed] });
+    console.error(error)
 };
