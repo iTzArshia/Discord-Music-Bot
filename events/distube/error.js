bffd4921adccde1d790be26331b9f5a059cd0650
iTz Arshia <89854127+iTzArshia@users.noreply.github.com>
2024-09-14 11:10:36 +0330

    Update error.js
    Add Title

--- events/distube/error.js
+++ events/distube/error.js
@@ -2,7 +2,10 @@ const Discord = require("discord.js");
 const config = require("../../config.json");
 
 module.exports = async (client, error, queue, song) => {
-    const embed = new Discord.EmbedBuilder().setColor(config.ErrorColor).setDescription(`An error encountered: ${error}`);
+    const embed = new Discord.EmbedBuilder()
+    .setColor(config.ErrorColor)
+    .setTitle("❌ Error")
+    .setDescription(`An error encountered: ${error}`);
     await queue.textChannel?.send({ embeds: [embed] });
     console.error(error)
 };
